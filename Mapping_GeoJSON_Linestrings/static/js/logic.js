038e093e3238d90abe206ddcaf907cea3b00eae4
Beardlow <bartlow.eric@gmail.com>
2022-09-08 12:18:12 -0400

    Linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -5,24 +5,24 @@ console.log("working");
 //let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+//let sanFranAirport =
+//{"type":"FeatureCollection","features":[{
+    //"type":"Feature",
+    //"properties":{
+        //"id":"3469",
+        //"name":"San Francisco International Airport",
+        //"city":"San Francisco",
+        //"country":"United States",
+        //"faa":"SFO",
+        //"icao":"KSFO",
+        //"alt":"13",
+        //"tz-offset":"-8",
+        //"dst":"A",
+        //"tz":"America/Los_Angeles"},
+        //"geometry":{
+            //"type":"Point",
+            //"coordinates":[-122.375,37.61899948120117]}}
+//]};
 
 // Grabbing our GeoJSON data.
 //L.geoJSON(sanFranAirport, {
@@ -36,12 +36,12 @@ let sanFranAirport =
 //});
 
 // Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+//let line = [
+    //[33.9416, -118.4085],
+    //[37.6213, -122.3790],
+    //[40.7899, -111.9791],
+    //[47.4502, -122.3088]
+  //];
 
   // Create a polyline using the line coordinates and make the line red.
 //L.polyline(line, {
@@ -49,7 +49,7 @@ let line = [
   //}).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+//let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 //cityData.forEach(function(city) {
@@ -64,7 +64,7 @@ let cityData = cities;
 //});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -84,7 +84,7 @@ let baseMaps = {
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44, -80],
   zoom: 2,
   layers: [streets]
 })
@@ -92,14 +92,29 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/Beardlow/Mapping_Earthquakes/main/torontoRoutes.json";
+
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Beardlow/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/Beardlow/Mapping_Earthquakes/main/majorAirports.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+    + feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
